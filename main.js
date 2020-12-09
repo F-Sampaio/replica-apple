012042e888f1edd782d71c53831e5faafba63e17
Franklin Sampaio <f.s1488@outlook.com>
2020-12-08 22:58:45 -0400

    copyright message

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ const selectElement = (element) => document.querySelector(element);
 selectElement('.mobile-menu').addEventListener('click', () => {
     selectElement('header').classList.toggle('active');
 });
+
+console.log('This site is for educational purposes only. \n All rights belong to their respective owners. I do not own any of this content.')
